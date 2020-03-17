4f335349eeb266052dcdcb5debf568026c1b4e95
Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>
2020-03-17 10:35:12 +0300

    chore (rest-api):
    1. JavaDocs were added to DefaultJobService

--- rest-api/src/main/java/org/klaster/restapi/service/DefaultJobService.java
+++ rest-api/src/main/java/org/klaster/restapi/service/DefaultJobService.java
@@ -19,26 +19,23 @@ import org.klaster.domain.exception.EmployerProfileNotFoundException;
 import org.klaster.domain.model.context.Job;
 import org.klaster.domain.model.context.User;
 import org.klaster.domain.model.entity.EmployerProfile;
-import org.klaster.domain.model.entity.FreelancerProfile;
 import org.klaster.domain.model.entity.JobMessage;
 import org.klaster.domain.model.entity.Skill;
 import org.klaster.domain.model.state.job.DeletedJobState;
 import org.klaster.domain.model.state.job.FinishedJobState;
 import org.klaster.domain.model.state.job.PublishedJobState;
 import org.klaster.domain.model.state.job.StartedJobState;
-import org.klaster.domain.repository.FreelancerProfileRepository;
 import org.klaster.domain.repository.JobMessageRepository;
 import org.klaster.domain.repository.JobRepository;
 import org.klaster.domain.repository.SkillRepository;
 import org.klaster.domain.util.MessageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * DefaultJobService
- *
+ * Provides interface for job management
  * @author Nikita Lepesevich
  */
 
@@ -56,16 +53,21 @@ public class DefaultJobService {
   private JobBuilder defaultJobBuilder;
 
   @Autowired
-  private FreelancerProfileRepository freelancerProfileRepository;
-
-  @Autowired
   private DefaultUserService defaultUserService;
 
   @Autowired
   private JobMessageRepository jobMessageRepository;
 
+  /**
+   * Creates job from passed jobDTO with passed user as author
+   *
+   * @param jobDTO - DTO of Job
+   * @param author - User that should be author
+   * @return created job
+   * @throws EmployerProfileNotFoundException if user has no employer profile
+   */
   @Transactional
-  public Job create(JobDTO jobDTO, User author) {
+  public Job create(JobDTO jobDTO, User author) throws EmployerProfileNotFoundException {
     EmployerProfile employerProfile = author.getCurrentState()
                                             .getEmployerProfile();
     if (employerProfile == null) {
@@ -80,6 +82,14 @@ public class DefaultJobService {
     return jobRepository.saveAndFlush(savedJob);
   }
 
+  /**
+   * Update job by passed id and passed user
+   * User should be owner of job
+   * @param jobId - updated job id
+   * @param jobDTO - job's DTO
+   * @param user - owner
+   * @return Updated job
+   */
   @Transactional
   public Job updateById(long jobId, JobDTO jobDTO, User user) {
     Job foundJob = findById(jobId);
@@ -93,15 +103,28 @@ public class DefaultJobService {
     return jobRepository.saveAndFlush(foundJob);
   }
 
+  /**
+   * Sets freelancer to job
+   * @param jobId updated job id
+   * @param userId id of user with freelancer profile
+   * @return updated job
+   */
   @Transactional
   public Job setFreelancerProfile(long jobId, long userId) {
     Job foundJob = findById(jobId);
     User foundUser = defaultUserService.findFirstById(userId);
     foundJob.getCurrentState()
-            .setFreelancerProfile(foundUser.getCurrentState().getFreelancerProfile());
+            .setFreelancerProfile(foundUser.getCurrentState()
+                                           .getFreelancerProfile());
     return jobRepository.saveAndFlush(foundJob);
   }
 
+  /**
+   * Changes state of job to started if job belongs to user and isn't started already
+   * @param jobId update job id
+   * @param user owner of job
+   * @return updated job
+   */
   @Transactional
   public Job startById(long jobId, User user) {
     Job foundJob = findById(jobId);
@@ -113,6 +136,12 @@ public class DefaultJobService {
     return foundJob;
   }
 
+  /**
+   * Changes job state to deleted if job belongs to user and isn't deleted already
+   * @param jobId updated job id
+   * @param user owner of job
+   * @return updated job
+   */
   @Transactional
   public Job deleteById(long jobId, User user) {
     Job foundJob = findById(jobId);
@@ -124,6 +153,12 @@ public class DefaultJobService {
     return foundJob;
   }
 
+  /**
+   * Changes state to finished of job if it isn't finished already and user is owner of job
+   * @param jobId updated job id
+   * @param user owner of job
+   * @return updated job
+   */
   @Transactional
   public Job finishById(long jobId, User user) {
     Job foundJob = findById(jobId);
@@ -135,11 +170,25 @@ public class DefaultJobService {
     return foundJob;
   }
 
+  /**
+   * Finds job by id
+   * @throws EntityNotFoundException if job with passed id doesn't exists
+   * @param jobId id of required job
+   * @return found job
+   */
   public Job findById(long jobId) {
     return jobRepository.findById(jobId)
                         .orElseThrow(() -> new EntityNotFoundException(MessageUtil.getEntityByIdNotFoundMessage(Job.class, jobId)));
   }
 
+  /**
+   * Adds message to job by user with freelancer profile
+   * @throws ActionForbiddenForUserException if user has already messaged to job
+   * @param jobId messaged job
+   * @param author author of message
+   * @param text text of message
+   * @return created message
+   */
   @Transactional
   public JobMessage addMessageFromUser(long jobId, User author, String text) {
     Job foundJob = findById(jobId);
@@ -152,30 +201,25 @@ public class DefaultJobService {
     return jobMessageRepository.save(jobMessage);
   }
 
+  /**
+   * Finds all published job
+   * @return published jobs, sorted by id
+   */
+  @Transactional
   public List<Job> findAll() {
-    return jobRepository.findAll()
+    List<Job> found =  jobRepository.findAll()
                         .stream()
                         .filter(job -> job.getCurrentState() instanceof PublishedJobState)
                         .sorted(Comparator.comparing(Job::getId))
                         .collect(Collectors.toList());
+    return found;
   }
 
-  private void validateJobDoesntBelongToUser(Job job, User user) {
-    EmployerProfile employerProfile = user.getCurrentState()
-                                          .getEmployerProfile();
-    if (null != employerProfile && job.belongsToEmployer(employerProfile.getId())) {
-      throw new ActionForbiddenForUserException();
-    }
-  }
-
-  private void validateJobBelongsToUser(Job job, User user) {
-    EmployerProfile employerProfile = user.getCurrentState()
-                                          .getEmployerProfile();
-    if (null == employerProfile || !job.belongsToEmployer(employerProfile.getId())) {
-      throw new EntityNotFoundException(MessageUtil.getEntityByParentIdNotFoundMessage(User.class, user.getId()));
-    }
-  }
-
+  /**
+   * Finds all messages of job by passed id
+   * @param jobId target job id
+   * @return messages of target job, sorted by id
+   */
   @Transactional
   public List<JobMessage> findAllMessagesByJobId(long jobId) {
     Job foundJob = findById(jobId);
@@ -184,4 +228,32 @@ public class DefaultJobService {
                    .sorted(Comparator.comparing(JobMessage::getId))
                    .collect(Collectors.toList());
   }
+
+  /**
+   * Validates user is not owner of job
+   *
+   * @throws ActionForbiddenForUserException if user is owner of job
+   */
+  private void validateJobDoesntBelongToUser(Job job, User user) throws ActionForbiddenForUserException {
+    EmployerProfile employerProfile = user.getCurrentState()
+                                          .getEmployerProfile();
+    if (null != employerProfile && job.belongsToEmployer(employerProfile.getId())) {
+      throw new ActionForbiddenForUserException();
+    }
+  }
+
+  /**
+   * Validates user is owner of job
+   * @throws EntityNotFoundException if user is not owner of job
+   * @param job
+   * @param user
+   */
+  private void validateJobBelongsToUser(Job job, User user) throws EntityNotFoundException {
+    EmployerProfile employerProfile = user.getCurrentState()
+                                          .getEmployerProfile();
+    if (null == employerProfile || !job.belongsToEmployer(employerProfile.getId())) {
+      throw new EntityNotFoundException(MessageUtil.getEntityByParentIdNotFoundMessage(User.class, user.getId()));
+    }
+  }
+
 }
